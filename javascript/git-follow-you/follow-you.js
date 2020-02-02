161bacb862d7093609fd54d74e90d00616a59490
Plops013 <pecorafatec@gmail.com>
2020-02-02 20:55:52 -0300

    ultimo commit

--- javascript/git-follow-you/follow-you.js
+++ javascript/git-follow-you/follow-you.js
@@ -2,8 +2,6 @@ const inputElement = document.getElementById('user');
 const listElement = document.querySelector("#app ul");
 
 //Função pegar array do git 
-
-
 function quemSigo(username) {
   return fetch(`https://api.github.com/users/${username}/following`);
 }
@@ -22,6 +20,7 @@ async function verificaCombinacao() {
   listElement.appendChild(loading);
 
   var user = inputElement.value;
+
   try {
     const sigoReq = await quemSigo(user);
     const sigoArray = await sigoReq.json();
@@ -34,18 +33,21 @@ async function verificaCombinacao() {
       for (var i = 0; i < segueArray.length; i++) {
         if (sigo.login === segueArray[i].login) {
           beleza.push(sigo);
+
           //var itemElement = document.createElement('li');
           //var okText = document.createTextNode(sigo.login);
           //itemElement.appendChild(okText);
           //listElement.appendChild(itemElement);
+
           break;
         } else if (i == segueArray.length - 1) {
 
           safados.push(sigo);
-          var itemElement = document.createElement('li');
 
+          var itemElement = document.createElement('li');
           var alert = document.createElement('span');
           alert.setAttribute('id', 'safado');
+
           var safadoSpan = document.createTextNode(" Não te segue");
           alert.appendChild(safadoSpan);
 
@@ -58,54 +60,17 @@ async function verificaCombinacao() {
         }
       }
     }
+    if (safados.length === 0) {
+      var itemElement = document.createElement('li');
+      var surpresaText = document.createTextNode("Ou você não segue ninguem, ou todo mundo te segue de volta :D");
+      itemElement.appendChild(surpresaText);
+      listElement.appendChild(itemElement);
+    }
   } catch (error) {
     erroItem = document.createElement('li');
     erroText = document.createTextNode("Usuario nao encontrado");
     erroItem.appendChild(erroText);
     listElement.appendChild(erroItem);
-    
+
   }
 }
-  /*
-quemSigo(u)
-.then(response => response.json())
-.then( sigo => {
-  console.log(sigo);
-  quemSegue(u)
-  .then(response => response.json())
-  .then( segue => {
-    console.log(segue);
-  });
-});
-}
-*/
-/*
-sigodores = sigodores.data;
-seguidores = seguidores.data;
-//console.log(seguidores);
-//console.log(sigodores);
-for (sigo of sigodores){
-  for (segue of seguidores){
-    if (sigo.login === segue.login){
-      //console.log(sigo);
-    } else {
-      //console.log(sigo);
-    }
-  }
-  //console.log(sigo.login);
-}
-}
-*/
-
-//console.log(verificaCombinacao());
-
-
-//Acessar quem eu sigo
-//pegar um vetor de seguidores baseado no login só
-
-//Acessar quem eles seguem
-//pegar um vetor de seguidores baseado no login só
-
-//Imprimir quem eu sigo que não me segue
-
-
