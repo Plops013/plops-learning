ddf338e98096d4d374b5f0c4d8363948d1efe231
Goufix <alifer.17.11@gmail.com>
2020-02-03 21:55:56 -0300

    Cria a funçao limpaLista e refatora o array safado

--- javascript/git-follow-you/follow-you.js
+++ javascript/git-follow-you/follow-you.js
@@ -1,4 +1,4 @@
-const inputElement = document.getElementById('user');
+const inputElement = document.getElementById("user");
 const listElement = document.querySelector("#app ul");
 
 function quemSigo(username) {
@@ -8,47 +8,53 @@ function quemSegue(username) {
   return fetch(`https://api.github.com/users/${username}/followers`);
 }
 
-function printaLista(value, safado){
-  const item = document.createElement('li');
+function printaLista(value, safado) {
+  const item = document.createElement("li");
   let texto = document.createTextNode(value);
   item.appendChild(texto);
 
-  if (safado){
-  const span = document.createElement('span');
-  const spanText = document.createTextNode(" não te segue");
-  span.setAttribute('id', 'safado');
-  span.appendChild(spanText);
-  item.appendChild(span);
+  if (safado) {
+    const span = document.createElement("span");
+    const spanText = document.createTextNode(" não te segue");
+    span.setAttribute("id", "safado");
+    span.appendChild(spanText);
+    item.appendChild(span);
   }
 
   listElement.appendChild(item);
+}
+
+const limparLista = () => {
+  listElement.innerHTML = "";
 };
 
 async function verificaCombinacao() {
   const user = inputElement.value;
-  listElement.innerHTML = "";
-  
+  limparLista();
+
   printaLista("carregando..");
 
   try {
     const sigoArray = await quemSigo(user).then(data => data.json());
     const segueArray = await quemSegue(user).then(data => data.json());
 
-    listElement.innerHTML = "";
-    let safados = [];
+    limparLista();
+    let safados = false;
 
     for (sigo of sigoArray) {
       for (let i = 0; i < segueArray.length; i++) {
         if (sigo.login === segueArray[i].login) {
           break;
         } else if (i == segueArray.length - 1) {
-          safados.push(sigo);
+          safados = true;
           printaLista(sigo.login, true);
         }
       }
     }
-    if (safados.length === 0) {
-      printaLista("Ou você não segue ninguem, ou todo mundo te segue de volta :D");
+    if (!safados) {
+      printaLista(
+        "Ou você não segue ninguem, ou todo mundo te segue de volta :D"
+      );
     }
   } catch (error) {
     printaLista(error);
